2e7dad28694c04bd7bc7c486e1d847d40f30d454
Owen Foreman <owen4man@gmail.com>
2022-05-15 23:09:26 -0400

    image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
+import image from "./IMG_0799.JPG"
 
 function App() {
   //about me
@@ -19,7 +20,21 @@ function App() {
         setActiveTab={setActiveTab}
       />
       <h2>{activeTab}</h2>
-      {activeTab == "About Me" && <p>Hello</p>}
+      {activeTab == "About Me" && (
+        <>
+        <img src={image}></img>
+      
+        <p>
+          My name is Owen Foreman and this is my first portfolio! I have joined
+          this program to further my knowledge of code and computers. Computers
+          are my biggest hobby and I want to know as much as I can. Anything
+          from gaming all night with my friends to researching new tech. I love
+          3d art, I'm getting into Blender and photoshop so I can further my
+          hobby. Hopefully by the end of this program I have the capabilites to
+          be a full stack web developer and can get my foot in the field!
+        </p>
+        </>
+      )}
       {activeTab == "Portfolio" && <p>HELOWERNN</p>}
       {activeTab == "Contact Me" && <p>Thi is me</p>}
       {activeTab == "Resume" && (
